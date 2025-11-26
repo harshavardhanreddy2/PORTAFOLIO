571860ad869f71c8de69bd1a0a7f4ea7edc118c0
harsha <kharshavardhanreddy423@gmail.com>
2025-11-26 14:30:23 +0530

    v3.0

--- mainpage.js
+++ mainpage.js
@@ -1,11 +1,11 @@
-function send(){
+function  toggleSend() {
     const send1 = document.querySelector('.sent');
 
-     if(send1.innerText === 'send'){
-        send1.innerHTML='sented';
-        send1.classList.add('is-send')
-    }else{
-        send1.innerHTML='send'
-        send1.classList.remove('is-send')
-        }        
+    if (send1.textContent === 'send') {
+        send1.textContent = 'sent';
+        send1.classList.add('is-send');
+    } else {
+        send1.textContent = 'send';
+        send1.classList.remove('is-send');
+    }
 }
